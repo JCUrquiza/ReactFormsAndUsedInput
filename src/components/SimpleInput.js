56530d27a153b9cf19e99e34abd40aecfe1542be
Juan <gurquiza8@gmail.com>
2023-06-04 22:09:06 -0500

    Manejando errores

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -4,6 +4,8 @@ const SimpleInput = (props) => {
 
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value);
@@ -12,17 +14,29 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = event => {
     event.preventDefault();
 
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() == '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+
+    setEnteredNameIsValid(true);
+
     console.log(enteredName);
     const enteredValue = nameInputRef.current.value;
     console.log(enteredValue);
 
     // nameInputRef.current.value = ''; => NOT IDEAL, DON´T MANIPULATED THE DOM
-    // setEnteredName('');
+    setEnteredName('');
   }
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control';
 
   return (
     <form onSubmit={ formSubmissionHandler }>
-      <div className='form-control'>
+      <div className={ nameInputClasses }>
         <label htmlFor='name'>Your Name</label>
         <input 
           ref={ nameInputRef } 
@@ -31,6 +45,9 @@ const SimpleInput = (props) => {
           onChange={ nameInputChangeHandler }
           value={ enteredName }
         />
+        {
+          nameInputIsInvalid && <p className='error-text'>Name must not be empty.</p>
+        }
       </div>
       <div className="form-actions">
         <button>Submit</button>
